27717fde5d3763df96909f25a151f6384b582310
Bob Melander <bob.melander@gmail.com>
2013-10-14 11:24:06 +0200

    Fix kwargs bug.
    Change-Id: I4f85d089fbab31e3d062f175c0cd3f49802e8b00

--- quantum/plugins/cisco/l3/common/service_vm_lib.py
+++ quantum/plugins/cisco/l3/common/service_vm_lib.py
@@ -168,12 +168,12 @@ class ServiceVMManager:
                                     tenant_id, max_hosted, **kwargs):
         if plugin == constants.N1KV_PLUGIN:
             return self.create_service_vm_resources_n1kv(
-                mgmt_nw_id, mgmt_sec_grp_id, tenant_id, max_hosted, kwargs)
+                mgmt_nw_id, mgmt_sec_grp_id, tenant_id, max_hosted, **kwargs)
         else:
             return self.create_service_vm_resources_ovs(
                 mgmt_nw_id, mgmt_sec_grp_id, tenant_id, max_hosted)
 
-    def create_service_vm_resources_n1kv(self, mgmt_nw_id, csr_mgmt_sec_grp_id,
+    def create_service_vm_resources_n1kv(self, mgmt_nw_id, mgmt_sec_grp_id,
                                          tenant_id, max_hosted, **kwargs):
         mgmt_port = None
         t1_n, t1_p, t2_n, t2_p = [], [], [], []
